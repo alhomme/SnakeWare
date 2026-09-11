ede1b46394cd73593a593c22f6d57dd70d87321f
alhomme <arnaud.lhomme00@gmail.com>
2026-09-11 16:37:49 +0200

    MiniGame Memory

---
+++ Assets/Scripts/MiniGame/MG_MemoryManager.cs
@@ -0,0 +1,286 @@
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MG_MemoryManager : MonoBehaviour
+{
+    [SerializeField] private Animator m_BallAnimator;
+    [SerializeField] private Animator m_IntroAnimator;
+    [SerializeField] private GameObject m_GoalPanel;
+
+    [SerializeField] private InputActionReference[] m_Inputs;
+    [SerializeField] private TextMeshProUGUI[] m_InputsText;
+
+
+    [SerializeField] private RSO_Speed m_SpeedRSO;
+    [SerializeField] private RSO_InputType m_InputTypeRSO;
+
+    [SerializeField] private RSE_LoadScene m_LoadSceneRSE;
+    [SerializeField] private RSE_SceneLoaded m_SceneLoadedRSE;
+    [SerializeField] private RSE_MG_Success m_SuccessRSE;
+    [SerializeField] private RSE_MG_Fail m_FailRSE;
+    [SerializeField] private RSE_PlaySFX m_PlaySFXRSE;
+
+    private string[] m_KeyboardInputs =
+        { "<sprite name=kd>", "<sprite name=kr>", "<sprite name=kl>", "<sprite name=ku>"};
+    private string[] m_GamepadInputs =
+        { "<sprite name=xa>", "<sprite name=xb>", "<sprite name=xx>", "<sprite name=xy>"};
+    private string[] m_UsedInputs;
+    private bool m_IsListening;
+    private int m_RandomInput;
+    private List<int> m_Sequence;
+    private int m_Count;
+    private int m_GoalCount;
+
+
+    private void OnEnable()
+    {
+        m_SceneLoadedRSE.Event += OnSceneLoaded;
+        m_InputTypeRSO.OnChanged += OnInputChanged;
+
+        m_Inputs[0].action.performed += OnButton1;
+        m_Inputs[1].action.performed += OnButton2;
+        m_Inputs[2].action.performed += OnButton3;
+        m_Inputs[3].action.performed += OnButton4;
+
+        m_RandomInput = 0;
+        m_Sequence = new List<int>();
+    }
+
+    private void OnDisable()
+    {
+        m_SceneLoadedRSE.Event -= OnSceneLoaded;
+        m_InputTypeRSO.OnChanged -= OnInputChanged;
+
+        m_Inputs[0].action.performed -= OnButton1;
+        m_Inputs[1].action.performed -= OnButton2;
+        m_Inputs[2].action.performed -= OnButton3;
+        m_Inputs[3].action.performed -= OnButton4;
+
+        StopAllCoroutines();
+    }
+
+    private void Start()
+    {
+        if (m_InputTypeRSO.Value == InputType.Keyboard)
+            m_UsedInputs = m_KeyboardInputs;
+        else if (m_InputTypeRSO.Value == InputType.Gamepad)
+            m_UsedInputs = m_GamepadInputs;
+
+        m_IsListening = false;
+
+        // Use RSO_Speed
+        m_Count = 0;
+        m_GoalCount = 2 + (int)m_SpeedRSO.Value;
+
+        //OnSceneLoaded("MiniGame_Memory");
+    }
+
+
+    private void OnSceneLoaded(string scene)
+    {
+        if (scene == "MiniGame_Memory")
+            StartCoroutine(ShowIntroPanel());
+    }
+
+    private IEnumerator ShowIntroPanel()
+    {
+        yield return new WaitForSeconds(1f);
+        m_IntroAnimator.SetTrigger("Start");
+        yield return new WaitForSeconds(1f);
+
+        StartCoroutine(StartSequence());
+    }
+
+    private IEnumerator StartSequence()
+    {
+        for (int i = 0; i < m_GoalCount; i++)
+        {
+            m_RandomInput = UnityEngine.Random.Range(0, 4);
+            Debug.Log("Show Input: " + m_RandomInput);
+            m_Sequence.Add(m_RandomInput);
+
+            m_InputsText[m_RandomInput].text = m_UsedInputs[m_RandomInput];
+            yield return new WaitForSeconds(0.7f);
+            m_InputsText[m_RandomInput].text = "";
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        // Whistle sfx
+        m_PlaySFXRSE.Dispatch("Whistle");
+        PrintSequence(m_Sequence);
+        m_IsListening = true;
+    }
+
+    private IEnumerator MG_Success()
+    {
+        Debug.Log("MiniGame Success");
+        m_IsListening = false;
+
+        // Play Success sfx
+
+        ClearInputsText();
+
+        m_PlaySFXRSE.Dispatch("Kick");
+        m_BallAnimator.SetTrigger("Success");
+        yield return new WaitForSeconds(1f);
+
+        m_PlaySFXRSE.Dispatch("CrowdGoal");
+        m_GoalPanel.SetActive(true);
+        yield return new WaitForSeconds(2f);
+
+        m_SuccessRSE.Dispatch();
+
+        // Return to Arena
+        m_LoadSceneRSE.Dispatch("Arena");
+        m_PlaySFXRSE.Dispatch("Stop");
+    }
+
+    private IEnumerator MG_Fail()
+    {
+        Debug.Log("MiniGame Fail");
+        m_IsListening = false;
+
+        // Add error sfx
+
+        ClearInputsText();
+
+        m_PlaySFXRSE.Dispatch("Kick");
+        m_BallAnimator.SetTrigger("Fail");
+        yield return new WaitForSeconds(0.5f);
+
+        m_PlaySFXRSE.Dispatch("CrowdBoo");
+        yield return new WaitForSeconds(3f);
+
+        m_FailRSE.Dispatch();
+
+        // Return to Arena
+        m_LoadSceneRSE.Dispatch("Arena");
+        m_PlaySFXRSE.Dispatch("Stop");
+
+    }
+
+    private void ClearInputsText()
+    {
+        foreach (var input in m_InputsText)
+        {
+            input.text = "";
+        }
+    }
+
+
+    private void OnButton1(InputAction.CallbackContext ctx)
+    {
+        if (m_IsListening)
+        {
+            ClearInputsText();
+            m_InputsText[0].text = m_UsedInputs[0];
+
+            if (m_Sequence[m_Count] == 0)
+            {
+                // Success
+                m_Count++;
+                if (m_Count == m_GoalCount)
+                    StartCoroutine(MG_Success());
+
+            }
+            else
+            {
+                // Fail
+                StartCoroutine(MG_Fail());
+            }
+        }
+    }
+
+    private void OnButton2(InputAction.CallbackContext ctx)
+    {
+        if (m_IsListening)
+        {
+            ClearInputsText();
+            m_InputsText[1].text = m_UsedInputs[1];
+
+            if (m_Sequence[m_Count] == 1)
+            {
+                // Success
+                m_Count++;
+                if (m_Count == m_GoalCount)
+                    StartCoroutine(MG_Success());
+
+            }
+            else
+            {
+                // Fail
+                StartCoroutine(MG_Fail());
+            }
+        }
+    }
+
+    private void OnButton3(InputAction.CallbackContext ctx)
+    {
+        if (m_IsListening)
+        {
+            ClearInputsText();
+            m_InputsText[2].text = m_UsedInputs[2];
+            if (m_Sequence[m_Count] == 2)
+            {
+                // Success
+                m_Count++;
+                if (m_Count == m_GoalCount)
+                    StartCoroutine(MG_Success());
+
+            }
+            else
+            {
+                // Fail
+                StartCoroutine(MG_Fail());
+            }
+        }
+    }
+
+    private void OnButton4(InputAction.CallbackContext ctx)
+    {
+        if (m_IsListening)
+        {
+            ClearInputsText();
+            m_InputsText[3].text = m_UsedInputs[3];
+            if (m_Sequence[m_Count] == 3)
+            {
+                // Success
+                m_Count++;
+                if (m_Count == m_GoalCount)
+                    StartCoroutine(MG_Success());
+
+            }
+            else
+            {
+                // Fail
+                StartCoroutine(MG_Fail());
+            }
+        }
+    }
+
+    private void OnInputChanged(InputType newInputType)
+    {
+        if (newInputType == InputType.Keyboard)
+            m_UsedInputs = m_KeyboardInputs;
+        else if (newInputType == InputType.Gamepad)
+            m_UsedInputs = m_GamepadInputs;
+    }
+
+    private void PrintSequence(List<int> sequence)
+    {
+        string str = "Sequence = { ";
+
+        foreach (int i in sequence)
+        {
+            str += i.ToString();
+            str += " ";
+        }
+        str += "}";
+
+        Debug.Log(str);
+    }
+}
--- Assets/Scripts/Technical/SceneLoader.cs
+++ Assets/Scripts/Technical/SceneLoader.cs
@@ -11,37 +11,44 @@ public class SceneLoader : MonoBehaviour
     [SerializeField] private RSE_LoadScene m_LoadSceneRSE;
     [SerializeField] private RSE_SceneLoaded m_SceneLoadedRSE;
     [SerializeField] private RSE_PlayMusic m_PlayMusicRSE;
+    [SerializeField] private RSE_Death m_DeathRSE;
+    [SerializeField] private RSE_NewGame m_NewGameRSE;
 
 
-    private string currentScene;
+    private string m_CurrentScene;
+    private bool m_IsDead;
 
 
     private void OnEnable()
     {
-        currentScene = "StarterScene";
+        m_CurrentScene = "StarterScene";
         m_LoadSceneRSE.Event += OnLoadScene;
+        m_DeathRSE.Event += OnDeath;
+        m_NewGameRSE.Event += OnNewGame;
     }
 
     private void OnDisable()
     {
-        m_LoadSceneRSE.Event += OnLoadScene;
+        m_LoadSceneRSE.Event -= OnLoadScene;
+        m_DeathRSE.Event -= OnDeath;
+        m_NewGameRSE.Event -= OnNewGame;
     }
 
     private void OnLoadScene(string scene)
     {
         // Load scene
-        Debug.Log("SceneLoader: Unload " + currentScene);
+        Debug.Log("SceneLoader: Unload " + m_CurrentScene);
         //SceneManager.UnloadSceneAsync(currentScene);
 
         Debug.Log("SceneLoader: Load: " + scene);
-        StartCoroutine(LoadNextScene(currentScene, scene));
-        currentScene = scene;
+        StartCoroutine(LoadNextScene(m_CurrentScene, scene));
+        m_CurrentScene = scene;
     }
 
     private IEnumerator LoadNextScene(string oldScene, string newScene)
     {
-        // Play Transition Except for MainMenu
-        if (newScene != "MainMenu")
+        // Play Transition except for MainMenu, or if snake is dead in MG
+        if (newScene != "MainMenu" && !m_IsDead)
         {
             m_Transition.SetTrigger("Start");
             // Wait
@@ -56,8 +63,8 @@ public class SceneLoader : MonoBehaviour
         yield return SceneManager.LoadSceneAsync(newScene, LoadSceneMode.Additive);
         SceneManager.SetActiveScene(SceneManager.GetSceneByName(newScene));
 
-        // Play Transition Except for MainMenu
-        if (newScene != "MainMenu")
+        // Play Transition Except for MainMenu, or if snake is dead in MG
+        if (newScene != "MainMenu" && !m_IsDead)
         {
             m_Transition.SetTrigger("End");
             // Wait
@@ -67,4 +74,15 @@ public class SceneLoader : MonoBehaviour
         // Add RSE SceneLoaded
         m_SceneLoadedRSE.Dispatch(newScene);
     }
+
+    private void OnDeath()
+    {
+        m_IsDead = true;
+    }
+
+    private void OnNewGame()
+    {
+        m_IsDead = false;
+    }
+
 }
